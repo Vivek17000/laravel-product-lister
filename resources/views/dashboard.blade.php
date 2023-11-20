d9bca7bdc83d40d95d03a67fca66afd37642ea0b
unknown <vivek17000@gmail.com>
2023-11-20 15:57:11 +0530

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+  
+@section('title', 'Dashboard - Laravel Admin Panel With Login and Registration')
+  
+@section('contents')
+  <div class="row">
+    Dashboard
+
+
+    <div>
+    <img src="https://www.datapine.com/blog/wp-content/uploads/2022/09/revenue-profit-margin-by-product.png" alt="no logo"> 
+
+   </div>
+  </div>
+@endsection
